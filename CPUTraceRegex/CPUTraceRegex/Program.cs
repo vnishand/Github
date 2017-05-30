abdc504011dabd77b1013df42d132139fae079a0
nishand <nishandv@hotmail.com>
2017-05-30 14:48:01 -0700

    Commit

---
+++ CPUTraceRegex/CPUTraceRegex/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUTraceRegex
+{
+    class Program
+    {
+        static List<byte[]> listArr = new List<byte[]>();
+        static void Main(string[] args)
+        {
+            int counter = 40;
+            while (counter-- > 0)
+            {
+                listArr.Add(new byte[80999990]);
+                System.Threading.Thread.Sleep(500);
+            }
+            throw new Exception();
+        }
+
+        static void RegularStatements(string StringValue)
+        {
+
+        }
+    }
+}
